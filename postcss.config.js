f4333f308abf66156c85ff0c715ad27e202dc044
Preetham K H <blighter.soul@gmail.com>
2026-01-12 17:08:33 +1100

    Add postCSS config - for tailwind

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+// PostCSS processes your CSS (Tailwind is a PostCSS plugin)
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {}, // Adds vendor prefixes automatically (-webkit-, -moz-, etc.)
+  },
+};
